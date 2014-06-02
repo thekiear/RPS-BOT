ea5a24bf7debc79e6f20ab7205781f98c64a3576
thekiear <kieransage@gmail.com>
2014-06-02 14:51:26 -0700

    began player_app

--- referee/referee/urls.py
+++ referee/referee/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'referee.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^player/', 'player.views.home', name='player_app_home')
     url(r'^match/', 'match.views.home_page', name="match_home"),
     url(r'^register/$','match.views.register', name='register'),
     url(r'^$', 'match.views.register', name="root_page"),
